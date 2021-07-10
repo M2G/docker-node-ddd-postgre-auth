67a3775c27b9bb26a4cabd7490f15035dc77e83e
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-07-10 04:27:53 +0200

    test refactor

--- test/api/register/post_register.spec.ts
+++ test/api/register/post_register.spec.ts
@@ -23,64 +23,72 @@ describe('Routes: POST Register', () => {
       .then((_: any) => done());
   });
 
-  it('should return authenticate user', (done) => {
-    rqt
-      .post(`/api/register`)
-      .send({
-        username: 'test',
-        password: 'test',
-      })
-      .expect(200)
-      .end((err: any, res: any) => {
-        expect(err).toBeNull();
-        expect(res.body.data).toHaveProperty('id');
-        expect(res.body.data).toHaveProperty('username', 'test');
-        expect(res.body.data).toHaveProperty('password');
-        done();
-      });
-  });
+  describe('Register user', () => {
+    beforeEach((done) => {
+      usersRepository
+        .destroy({ where: {} })
+        .then(() => done())
+    })
 
-  it('shouldnt register user return error empty username was sent', (done) => {
-    rqt
-      .post(`/api/register`)
-      .send({
-        username: '',
-        password: 'gesdf',
-      })
-      .expect(422)
-      .end((err: any, res: any) => {
-        expect(err).toBeNull();
-        expect(res.body.success).toBeFalsy();
-        expect(res.body.error).toEqual('Empty username.');
-        done();
-      });
-  });
+    it('should return register user', (done) => {
+      rqt
+        .post(`/api/register`)
+        .send({
+          username: 'test',
+          password: 'test',
+        })
+        .expect(200)
+        .end((err: any, res: any) => {
+          expect(err).toBeFalsy();
+          expect(res.body.data).toHaveProperty('id');
+          expect(res.body.data).toHaveProperty('username', 'test');
+          expect(res.body.data).toHaveProperty('password');
+          done();
+        });
+    });
 
-  it('shouldnt register user return error empty password was sent', (done) => {
-    rqt
-      .post(`/api/register`)
-      .send({
-        username: 'gesdf',
-        password: '',
-      })
-      .expect(422)
-      .end((err: any, res: any) => {
-        expect(err).toBeNull();
-        expect(res.body.success).toBeFalsy();
-        expect(res.body.error).toEqual('Empty password.');
-        done();
-      });
-  });
+    it('shouldnt register user return error empty username was sent', (done) => {
+      rqt
+        .post(`/api/register`)
+        .send({
+          username: '',
+          password: 'gesdf',
+        })
+        .expect(422)
+        .end((err: any, res: any) => {
+          expect(err).toBeFalsy();
+          expect(res.body.success).toBeFalsy();
+          expect(res.body.error).toEqual('Empty username.');
+          done();
+        });
+    });
 
-  it('shouldnt register user return error empty body was sent', (done) => {
-    rqt
-      .post(`/api/register`)
-      .send({})
-      .expect(422)
-      .end((err: any, res: any) => {
-        expect(err).toBeNull();
-        expect(res.body.success).toBeFalsy();
-        done();
-      });
+    it('shouldnt register user return error empty password was sent', (done) => {
+      rqt
+        .post(`/api/register`)
+        .send({
+          username: 'gesdf',
+          password: '',
+        })
+        .expect(422)
+        .end((err: any, res: any) => {
+          expect(err).toBeFalsy();
+          expect(res.body.success).toBeFalsy();
+          expect(res.body.error).toEqual('Empty password.');
+          done();
+        });
+    });
+
+    it('shouldnt authenticate user return error empty body was sent', (done) => {
+      rqt
+        .post(`/api/register`)
+        .send({})
+        .expect(422)
+        .end((err: any, res: any) => {
+          expect(err).toBeFalsy();
+          expect(res.body.success).toBeFalsy();
+          done();
+        });
+    });
   });
 });
---
+++ test/unit/app/register/post_register.spec.ts
@@ -0,0 +1,57 @@
+import postUsecase from  '../../../../src/app/register/post';
+
+describe('App -> User -> Post', () => {
+  let useCase: { register: any; };
+
+  describe('Success path', () => {
+    beforeEach(() => {
+      const MockRepository = {
+        register: (data: any) => data
+      }
+
+      useCase = postUsecase({
+        usersRepository: MockRepository
+      })
+    })
+
+    it('test', async () => {
+      const body = {
+        username: 'test',
+        password: 'test',
+      }
+
+      const lists = await useCase.register({ body })
+      expect(lists.username).toEqual(body.username)
+      expect(lists.password).toEqual(body.password)
+    })
+  })
+
+  describe('Fail path', () => {
+    const body = {
+      username: 'test',
+      password: 'test',
+    }
+
+    beforeEach(() => {
+      const MockRepository = {
+        register: () => Promise.reject('Error')
+      }
+
+      useCase = postUsecase({
+        usersRepository: MockRepository
+      })
+    })
+
+    it('should display error on rejection', async () => {
+
+      let error
+      try {
+        await useCase.register({ body })
+      } catch (e) {
+        error = e.message
+      }
+      expect(error).toEqual('Error')
+    })
+  })
+
+})
