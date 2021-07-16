f59d11e3faa7207b13f33a1cd4091b649e9a890f
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-07-16 02:23:27 +0200

    add test should return users list

--- src/infra/jwt/jwt.ts
+++ src/infra/jwt/jwt.ts
@@ -10,12 +10,12 @@ import {
 export default ({ config }: any) => ({
   signin: (options?: any) => (payload: string | object | Buffer) => {
     const opt = Object.assign({}, options, { expiresIn: 60 * 60 });
-    return jwt.sign(payload, config.authSecret as string, opt);
+    return jwt.sign(payload, config.authSecret as string || 'SECRET', opt);
   },
   verify: (options?: any) => (token: string) => {
     const opt = Object.assign({}, options);
 
-    return jwt.verify(token, config.authSecret as string, opt);
+    return jwt.verify(token, config.authSecret as string || 'SECRET', opt);
   },
   decode: (options?: any) => (token: any) => {
     const opt = Object.assign({}, options);
---
+++ test/api/users/get_users.spec.ts
@@ -0,0 +1,58 @@
+/* eslint-disable */
+import request from 'supertest';
+import container from '../../../src/container';
+
+const server: any = container.resolve('server');
+
+const rqt: any = request(server.app);
+
+const { usersRepository } = container.resolve('repository');
+
+describe('Routes: POST Register', () => {
+  // const BASE_URI = '/api';
+  // @ts-ignore
+  const signIn = container.resolve('jwt').signin();
+  let token: any;
+
+  beforeEach((done) => {
+    // we need to add user before we can request our token
+    usersRepository
+      .destroy({ where: {} })
+      .then(() =>
+        usersRepository.register({
+          username: 'test',
+          password: 'test',
+        }),
+      )
+      .then(() =>
+        usersRepository.register({
+          username: 'test2',
+          password: 'test2',
+        }),
+      )
+      .then((user: { id: any; username: any; }) => {
+        token = signIn({
+          id: user.id,
+          username: user.username,
+        });
+
+        console.log('token token token token', token)
+
+        done();
+      })
+  });
+
+  describe('Register user', () => {
+    it('should return users list', (done) => {
+      rqt
+        .get(`/api/users`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .end((err: any, res: any) => {
+          expect(err).toBeFalsy();
+          expect(res.body.data.length).toEqual(2);
+          done(err)
+        });
+    });
+  });
+});
