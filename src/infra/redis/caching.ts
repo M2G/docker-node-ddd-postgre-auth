89dc22646bf8ad15e6ad55b2d6ebe726785c71f7
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-07-31 20:13:00 +0200

    add redis store (wip)

--- src/infra/redis/caching.ts
+++ src/infra/redis/caching.ts
@@ -1,11 +1,15 @@
 /*eslint-disable*/
 import * as redis from 'redis';
 import Status from 'http-status';
+import validatedTtl from './validatedTtl';
 
 const portRedis = process.env.PORT_REDIS ?? '6379';
 
 const redisClient = redis.createClient(portRedis);
 
+let defaultTtlInS = validatedTtl(null);
+
+/*
 const set = (key: string, value: any) =>
   redisClient.set(key, JSON.stringify(value));
 
@@ -19,6 +23,44 @@ const get = (req: any, res: any, next: any) => {
     else return next();
   });
 }
+*/
+/**
+ * Return the defaultTtlInS
+ * @returns defaultTtlInS
+ */
+function getDefaultTtlInS(): number | undefined {
+  return defaultTtlInS
+}
+
+/**
+ * Sets the defaultTtlInS
+ * @param ttl
+ * @returns defaultTtlInS
+ */
+function setDefaultTtlInS(ttl: number): number | undefined {
+  defaultTtlInS = validatedTtl(ttl)
+  return defaultTtlInS
+}
+
+/**
+ * Unsets the defaultTtlInS
+ * @param ttl
+ * @returns true
+ */
+function unsetDefaultTtlInS(): boolean {
+  defaultTtlInS = undefined
+  return true
+}
+
+/**
+ * Returns 'PONG'
+ *
+ * @param str - string passed
+ * @returns 'PONG'/string passed
+ */
+function ping(str?: string): Promise<string> {
+  return super.sendCommand('ping', str ? [str] : [])
+}
 
 /**
  * Returns value or null when the key is missing - See [redis get]{@link https://redis.io/commands/get}
@@ -27,7 +69,7 @@ const get = (req: any, res: any, next: any) => {
  * @returns value or null when the key is missing
  */
 const get2 = async (key: string):Promise<any> => {
-  let result = await super.sendCommand('get', [key])
+  let result = await redisClient.set('get', [key])
 
   try {
     result = JSON.parse(result)
@@ -45,7 +87,7 @@ const get2 = async (key: string):Promise<any> => {
  * @param ttlInSeconds - time to live in seconds
  * @returns 'OK' if successful
  */
-const set2 = (key: string, value: any, ttlInSeconds?: number): Promise<any> => {
+function set2(key: string, value: any, ttlInSeconds?: number): Promise<any> {
   const str =
     Array.isArray(value) || isJSON(value, true)
       ? JSON.stringify(value)
@@ -56,6 +98,91 @@ const set2 = (key: string, value: any, ttlInSeconds?: number): Promise<any> => {
     return super.sendCommand('setex', [key, ttl, str])
   }
   return super.sendCommand('set', [key, str])
+}
+
+/**
+ * Returns 'OK' if successful
+ * @async
+ * @param key          - key for the value stored
+ * @param value        - value to stored
+ * @param ttlInSeconds - time to live in seconds
+ * @returns
+ */
+async function getset(
+  key: string,
+  value: any,
+  ttlInSeconds: number | undefined
+): Promise<any> {
+  const str =
+    Array.isArray(value) || isJSON(value, true)
+      ? JSON.stringify(value)
+      : value
+  const ttl = validatedTtl(ttlInSeconds, this.defaultTtlInS)
+
+  let result = await super.sendCommand('getset', [key, str])
+  try {
+    result = JSON.parse(result)
+  } catch (e) {
+    // do nothing
+  }
+
+  if (ttl) {
+    await super.sendCommand('expire', [key, ttl])
+  }
+  return result
+}
+
+/**
+ * Returns the number of keys that were removed - See [redis del]{@link https://redis.io/commands/del}
+ *
+ * @param keys - list of keys to delete
+ * @returns The number of keys that were removed.
+ */
+function del(keys: string[] = []): Promise<number> {
+  return super.sendCommand('del', keys)
+}
+
+/**
+ * Returns 1 if the timeout was set/ 0 if key does not exist or the timeout could not be set - See [redis expire]{@link https://redis.io/commands/expire}
+ *
+ * @param   {string}  key          - key to set expire
+ * @param   {number}  ttlInSeconds - time to live in seconds
+ * @returns 1 if the timeout was set successfully; if not 0
+ */
+function expire(key: string, ttlInSeconds: number): Promise<number> {
+  const ttl = validatedTtl(ttlInSeconds)
+  return super.sendCommand('expire', [key, ttl])
+}
+
+/**
+ * Returns TTL in seconds, or a negative value in order to signal an error - See [redis ttl]{@link https://redis.io/commands/ttl}
+ *
+ * @param key - list of keys to delete
+ * @returns TTL in seconds, or a negative value in order to signal an error
+ */
+function getTtl(key: string): Promise<number> {
+  return super.sendCommand('ttl', [key])
+}
+
+/**
+ * Returns all keys matching pattern - See [redis keys]{@link https://redis.io/commands/keys}
+ *
+ * @param pattern - glob-style patterns/default '*'
+ * @returns all keys matching pattern
+ */
+function keys(pattern = '*'): Promise<string[]> {
+  return super.sendCommand('keys', [pattern])
+}
+
+/**
+ * Deletes all keys matching pattern
+ *
+ * @param pattern - glob-style patterns/default '*'
+ * @returns The number of keys that were removed.
+ */
+function deleteAll(pattern = '*'): Promise<number> {
+  debug('clearing redis keys: ', pattern)
+return this._executeDeleteAll(pattern)
 }
 
 
