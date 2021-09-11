34bc7ac42d641d01f9b56b2b08574a9270a03e92
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-09-12 00:20:38 +0200

    drop ramda (wip)

--- src/infra/support/response.ts
+++ src/infra/support/response.ts
@@ -1,18 +1,16 @@
 /*eslint-disable*/
 export default ({ config }: any) => {
   const defaultResponse = (success = true) => ({
-      success,
-      version: config.version,
-      date: new Date()
+    success,
+    version: config.version,
+    date: new Date()
   })
 
-  const Success = (data: any) => {
-    return { ...defaultResponse(true), data };
-  }
+  const Success = (data: any) =>
+    ({ ...defaultResponse(true), data });
 
-  const Fail = (data: any) => {
-    return { ...defaultResponse(false), error: data };
-  }
+  const Fail = (data: any) =>
+    ({ ...defaultResponse(false), error: data });
 
   return {
     Success,
