353948a45634e30f2b7a92fa9ea12ab5c9d81345
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2021-11-23 02:24:20 +0100

    update domain/users

--- src/domain/users/users.ts
+++ src/domain/users/users.ts
@@ -2,9 +2,11 @@ import t from 'tcomb';
 
 const Users = t.struct({
   _id: t.maybe(t.Object),
+  createdAt: t.maybe(t.Date),
   email: t.maybe(t.String),
   firstName: t.maybe(t.String),
   lastName: t.maybe(t.String),
+  modifiedAt: t.maybe(t.Date),
   password: t.maybe(t.String),
   username: t.maybe(t.String),
 });
