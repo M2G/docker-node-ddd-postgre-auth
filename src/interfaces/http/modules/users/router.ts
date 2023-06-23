c0b5b7ea9c3b59f47d77a82896c36f81c1ae7f90
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-23 05:02:14 +0200

    feat: refactor and rm mongo part form delete users

--- src/interfaces/http/modules/users/router.ts
+++ src/interfaces/http/modules/users/router.ts
@@ -2,7 +2,6 @@
 import Status from 'http-status';
 import { Router, Request, Response, NextFunction } from 'express';
 import IUser from 'core/IUser';
-import { isValidObjID } from 'interfaces/http/utils';
 
 export default ({
   getOneUseCase,
@@ -29,7 +28,7 @@ export default ({
       );
 
       res.status(Status.OK).json(Success(data));
-    } catch (error: any) {
+    } catch (error) {
       logger.error(error);
       res.status(Status.BAD_REQUEST).json(Fail(error.message));
     }
@@ -39,13 +38,13 @@ export default ({
     const { params } = req;
     const { id } = params;
 
-    if (!isValidObjID(id))
+    if (!id)
       return res
         .status(Status.UNPROCESSABLE_ENTITY)
         .json(Fail('Invalid id parameters in request.'));
 
     try {
-      const data = await getOneUseCase.getOne({ _id: req.params.id });
+      const data = await getOneUseCase.getOne({ id });
       logger.debug(data);
       return res.status(Status.OK).json(Success(data));
     } catch (error: any) {
@@ -64,16 +63,16 @@ export default ({
 
     console.log('values values values values values values', values);
 
-    if (!isValidObjID(id) || values)
+    if (!id || values)
       return res.status(Status.UNPROCESSABLE_ENTITY).json(Fail('Invalid parameters in request.'));
 
     try {
       const updateValue: IUser = {
         ...body,
-        modified_at: Math.floor(Date.now() / 1000),
+        modified_at: Date.now(),
       };
 
-      const data = await putUseCase.update({ _id: id, ...updateValue });
+      const data = await putUseCase.update({ id, ...updateValue });
       logger.debug(data);
       if (!data) return res.status(Status.NOT_FOUND).json(Fail());
       return res.status(Status.OK).json(Success());
@@ -87,15 +86,13 @@ export default ({
     const { params } = req;
     const { id } = params;
 
-    console.log('DELETE', id);
-
-    if (!isValidObjID(id))
+    if (!id)
       return res
         .status(Status.UNPROCESSABLE_ENTITY)
         .json(Fail('Invalid id parameters in request.'));
 
     try {
-      const data = await deleteUseCase.remove({ _id: id });
+      const data = await deleteUseCase.remove({ id });
       logger.debug(data);
       if (!data) return res.status(Status.NOT_FOUND).json(Fail());
       return res.status(Status.OK).json(Success());
